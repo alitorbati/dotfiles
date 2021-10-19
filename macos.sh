2859283fd12d86e7034ff64cda4d0cbdf1335059
Ali Torbati <ali.torbati@gmail.com>
2021-10-18 21:25:47 -0700

    disable keep-alive

--- macos.sh
+++ macos.sh
@@ -4,7 +4,7 @@ echo -e "🚀 running"
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until this script has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
 # ==========
