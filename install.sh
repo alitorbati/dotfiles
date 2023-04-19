3676d3e5cf4df96724bfde2fead32b76918f31c1
Ali Torbati <ali.torbati@gmail.com>
2023-04-19 16:28:03 -0700

    more symlinks

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 rm ~/.zshrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.aliases ~/.aliases
+ln -s ~/dotfiles/.hushlogin ~/.hushlogin
 ln -s /workspace/container-data/.zshrc.local ~/.zshrc.local
