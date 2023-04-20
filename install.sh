a484b4e078871ccbce02140ef96122a2b73de8f7
Ali Torbati <ali.torbati@gmail.com>
2023-04-19 22:04:28 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 rm ~/.zshrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
-
 ln -s ~/dotfiles/.aliases ~/.aliases
-
 ln -s ~/dotfiles/.hushlogin ~/.hushlogin
 
-rm ~/.gitconfig
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-
+ln -s /workspace/container-data/.gitconfig.local ~/.gitconfig.local
 ln -s /workspace/container-data/.zshrc.local ~/.zshrc.local
