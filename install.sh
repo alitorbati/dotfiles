c463e045395f6556cf77a8f9f3a236aabdd79c0b
Ali Torbati <ali.torbati@gmail.com>
2024-02-23 15:08:40 -0800

    history test

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ ln -s ~/dotfiles/.hushlogin ~/.hushlogin
 
 ln -s /workspace/container-data/.gitconfig.local ~/.gitconfig.local
 ln -s /workspace/container-data/.zshrc.local ~/.zshrc.local
+ln -s /workspace/container-data/.bash_history ~/.bash_history
